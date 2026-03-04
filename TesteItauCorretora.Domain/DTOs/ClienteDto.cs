4610565834cfb37fa0cec84da687fa4c1d895bb4
Victor Toledo <victor.toledo36@hotmail.com>
2026-03-04 02:02:45 -0300

    feat: Criando DtoCliente

---
+++ TesteItauCorretora.Domain/DTOs/ClienteDto.cs
@@ -0,0 +1,28 @@
+﻿using TesteItauCorretora.Domain.Entities;
+
+namespace TesteItauCorretora.Core.UseCases.Clientes;
+
+public class ClienteDto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string CPF { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public decimal ValorMensal { get; set; }
+    public bool Ativo { get; set; }
+    public DateTime DataAdesao { get; set; }
+
+    public static ClienteDto FromEntity(Cliente cliente)
+    {
+        return new ClienteDto
+        {
+            Id = cliente.Id,
+            Nome = cliente.Nome,
+            CPF = cliente.CPF,
+            Email = cliente.Email,
+            ValorMensal = cliente.ValorMensal,
+            Ativo = cliente.Ativo,
+            DataAdesao = cliente.DataAdesao
+        };
+    }
+}
