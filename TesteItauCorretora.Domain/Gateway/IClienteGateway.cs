84560c7047a9e8b01c9d75d605bdb7831e093f46
Victor Toledo <victor.toledo36@hotmail.com>
2026-03-04 02:04:32 -0300

    feat: Criando as interfaces Gateways e os UseCases

---
+++ TesteItauCorretora.Domain/Gateway/IClienteGateway.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteItauCorretora.Domain.Entities;
+
+namespace TesteItauCorretora.Core.Gateway
+{
+    interface IClienteGateway
+    {
+        Task<Cliente> ObterClientePorCpf(string cpf);
+        Task<Cliente> ObterClientePorEmail(string email);
+        Task<Cliente> ObterClientePorId(Guid id);
+        Task CadastrarCliente(Cliente cliente);
+        Task AtualizarCliente(Cliente cliente);
+    }
+}
---
+++ TesteItauCorretora.Domain/Gateway/IContaGraficaGateway.cs
@@ -0,0 +1,12 @@
+﻿using TesteItauCorretora.Domain.Entities;
+
+namespace TesteItauCorretora.Core.Gateway;
+
+public interface IContaGraficaGateway
+{
+    Task Add(ContaGrafica conta);
+
+    Task<ContaGrafica?> ObterPorId(int id);
+
+    Task<ContaGrafica?> ObterPorClienteId(int clienteId);
+}
