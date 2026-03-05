4ad867924e67e88736bee2955a79fdcc9d4ea124
Victor Toledo <victor.toledo36@hotmail.com>
2026-03-04 22:58:27 -0300

    feat: Implementando Adicionar e Remover ativos na classe custodia

--- TesteItauCorretora.Domain/Domain/Entities/Custodia.cs
+++ TesteItauCorretora.Domain/Domain/Entities/Custodia.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace TesteItauCorretora.Domain.Entities;
@@ -10,11 +9,33 @@ public class Custodia
 {
     public int Id { get; set; }
     public int ContaGraficaId { get; set; }
-    public ContaGrafica ContaGrafica { get; private set; }
+    public ContaGrafica ContaGrafica { get; private set; } = null!;
     public string Ticker { get; set; } = string.Empty;
     public int Quantidade { get; set; }
     public decimal PrecoMedio { get; set; }
     public DateTime DataUltimaAtualizacao { get; set; }
     public ICollection<Distribuicao> Distribuicao { get; set; } = new List<Distribuicao>();
 
+    public void AdicionarAtivos(int quantidade, decimal precoUnitario)
+    {
+        if (quantidade <= 0)
+            throw new ArgumentException("Quantidade deve ser maior que zero.");
+
+        var valorTotal = (Quantidade * PrecoMedio) + (quantidade * precoUnitario);
+        Quantidade += quantidade;
+        PrecoMedio = valorTotal / Quantidade;
+        DataUltimaAtualizacao = DateTime.Now;
+    }
+
+    public void RemoverAtivos(int quantidade)
+    {
+        if (quantidade <= 0)
+            throw new ArgumentException("Quantidade deve ser maior que zero.");
+
+        if (quantidade > Quantidade)
+            throw new InvalidOperationException("Quantidade insuficiente em custódia.");
+
+        Quantidade -= quantidade;
+        DataUltimaAtualizacao = DateTime.Now;
+    }
 }
