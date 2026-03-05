bff91fae7b4e134d1a86805ae21fd049bbd38c2b
Victor Toledo <victor.toledo36@hotmail.com>
2026-03-04 23:31:44 -0300

    feat: Adicionando using necessárias

--- TesteItauCorretora.Domain/Domain/Entities/EventoIR.cs
+++ TesteItauCorretora.Domain/Domain/Entities/EventoIR.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace TesteItauCorretora.Domain.Entities;
 
 public enum TipoEventoIR
 {
-    DedoDuro = 1,
+    Compra = 1,
     Venda = 2
 }
 
@@ -16,9 +15,11 @@ public class EventoIR
 {
     public int Id { get; set; }
     public int ClienteId { get; set; }
-    public TipoEventoIR Tipo { get; set; }
-    public decimal ValorBase { get; set; }
+    public string Ticker { get; set; } = string.Empty;
+    public int Quantidade { get; set; }
+    public decimal ValorOperacao { get; set; }
     public decimal ValorIR { get; set; }
+    public TipoEventoIR TipoEvento { get; set; }
     public bool PublicadoKafka { get; set; }
     public DateTime DataEvento { get; set; }
 }
