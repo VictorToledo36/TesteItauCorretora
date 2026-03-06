b9c9227581b2b91a3dd444730b3137d5cfa196bd
Victor Toledo <victor.toledo36@hotmail.com>
2026-03-06 00:25:20 -0300

    feat: Implementando novas interfaces no arquivo program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using TesteItauCorretora.Infrastructure.Persistence;
 using TesteItauCorretora.Domain.Gateway;
 using TesteItauCorretora.Infrastructure.Gateway;
+using TesteItauCorretora.Domain.UseCase.Clientes;
+using TesteItauCorretora.Domain.UseCase.Adm;
 using TesteItauCorretora.Core.UseCase.Clientes;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,9 +23,18 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Registrar Repositories
 builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
 builder.Services.AddScoped<IContaGraficaRepository, ContaGraficaRepository>();
+builder.Services.AddScoped<ICestaRecomendacaoRepository, CestaRecomendacaoRepository>();
+builder.Services.AddScoped<ICustodiaRepository, CustodiaRepository>();
+builder.Services.AddScoped<ICotacaoRepository, CotacaoRepository>();
+builder.Services.AddScoped<IDistribuicaoRepository, DistribuicaoRepository>();
 
 // Registrar UseCases
 builder.Services.AddScoped<AdesaoClienteUseCase>();
+builder.Services.AddScoped<CadastrarCestaUseCase>();
+builder.Services.AddScoped<SaidaClienteUseCase>();
+builder.Services.AddScoped<AlterarValorMensalUseCase>();
+builder.Services.AddScoped<ConsultarCarteiraUseCase>();
+builder.Services.AddScoped<ConsultarRentabilidadeUseCase>();
 
 var app = builder.Build();
 
