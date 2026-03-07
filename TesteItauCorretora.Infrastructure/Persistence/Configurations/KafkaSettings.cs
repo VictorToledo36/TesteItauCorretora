152472bec7417040a539609fb8fd32ef2059ba5b
Victor Toledo <victor.toledo36@hotmail.com>
2026-03-07 00:06:01 -0300

    feat: Configurando kafka na infra

---
+++ TesteItauCorretora.Infrastructure/Persistence/Configurations/KafkaSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace TesteItauCorretora.Infrastructure.Configuration;
+
+public class KafkaSettings
+{
+    public string BootstrapServers { get; set; } = string.Empty;
+    public string ApiKey { get; set; } = string.Empty;
+    public string ApiSecret { get; set; } = string.Empty;
+    public string TopicoIR { get; set; } = string.Empty;
+}
