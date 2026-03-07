717460acfdb2f8609160fe0c3654d3d9b609967d
Victor Toledo <victor.toledo36@hotmail.com>
2026-03-07 00:05:07 -0300

    feat: Implementando use case ConsultarHistorico

---
+++ TesteItauCorretora.Domain/UseCase/Adm/ConsultarHistoricoCestasUseCase.cs
@@ -0,0 +1,35 @@
+﻿using TesteItauCorretora.Domain.DTOs.Response;
+using TesteItauCorretora.Domain.Gateway;
+
+namespace TesteItauCorretora.Domain.UseCases;
+
+public class ConsultarHistoricoCestasUseCase
+{
+    private readonly ICestaRecomendacaoRepository _cestaRepository;
+
+    public ConsultarHistoricoCestasUseCase(ICestaRecomendacaoRepository cestaRepository)
+    {
+        _cestaRepository = cestaRepository;
+    }
+
+    public async Task<HistoricoCestasResponse> ExecutarAsync()
+    {
+        var cestas = await _cestaRepository.ObterHistoricoAsync();
+
+        var lista = cestas.Select(c => new CestaHistoricoDto
+        {
+            CestaId = c.Id,
+            Nome = c.Nome,
+            Ativa = c.Ativa,
+            DataCriacao = c.DataCriacao,
+            DataDesativacao = c.DataDesativacao,
+            Itens = c.Itens.Select(i => new ItemCestaDto
+            {
+                Ticker = i.Ticker,
+                Percentual = i.Percentual
+            }).ToList()
+        }).ToList();
+
+        return new HistoricoCestasResponse { Cestas = lista };
+    }
+}
