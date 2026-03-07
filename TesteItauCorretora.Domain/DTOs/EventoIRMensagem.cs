a26ea07ed719ec50218f7a7a341f6425c605090f
Victor Toledo <victor.toledo36@hotmail.com>
2026-03-07 00:04:14 -0300

    feat: Adc Classe EventoIRMensagem

---
+++ TesteItauCorretora.Domain/DTOs/EventoIRMensagem.cs
@@ -0,0 +1,12 @@
+﻿namespace TesteItauCorretora.Domain.DTOs;
+public class EventoIRMensagem
+{
+    public int ClienteId { get; set; }
+    public string CPF { get; set; } = string.Empty;
+    public string Ticker { get; set; } = string.Empty;
+    public int Quantidade { get; set; }
+    public decimal ValorOperacao { get; set; }
+    public decimal ValorIR { get; set; }
+    public string TipoEvento { get; set; } = string.Empty; 
+    public DateTime DataEvento { get; set; }
+}
